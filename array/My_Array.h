bae79e7142902d8a31d857bf71be3717f5da9403
delsin97 <919885558@qq.com>
2021-06-15 11:30:01 +0800

    array

---
+++ array/My_Array.h
@@ -0,0 +1,17 @@
+#ifndef MY_ARRAY_H
+#define MY_ARRAY_H
+//dynamic array -- vector in C++
+class MyArray
+{
+public:
+    MyArray(int capacity);
+    ~MyArray();
+    int size();
+    int capacity();
+    bool is_empty();
+
+private:
+
+};
+
+#endif
